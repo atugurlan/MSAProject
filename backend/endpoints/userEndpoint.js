22cb7a0766f825673f8bfb19f65a88a90068291a
atugurlan <tugurlan.alexandra@gmail.com>
2024-11-18 06:29:13 +0200

    change password feature works + changed some dependencies

--- backend/endpoints/userEndpoint.js
+++ backend/endpoints/userEndpoint.js
@@ -36,4 +36,40 @@ router.post("/users/login", async (req, res) => {
     }
 });
 
+
+router.put('/users/changePassword', async (req, res) => {
+    const { email, password, confirmPassword } = req.body;
+
+    if(!email || !password || !confirmPassword) {
+        return res.status(400).json({ message: 'All fiels are required and should be filled.'})
+    }
+
+    try {
+        const userRows = await pool.query( 
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        );
+
+        if( userRows.rowCount !== 1 ) {
+            res.status(400).json({ message: "User does not exist" });
+        }
+
+        const user = userRows.rows[0];
+
+        if( password !== confirmPassword ) {
+            res.status(400).json({ message: "The new password should match with the confirm password" });
+        }
+
+        const result = await pool.query(
+            "UPDATE users SET password=$1 WHERE email=$2",
+            [password, user.email]
+        );
+
+        res.status(200).json({ message: "Successfully changed password" });
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: "Failed to change password" })
+    }
+})
+
 module.exports = router;
